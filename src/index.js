1a0c9650ea73373d2bf56ed6c48ce4a1fcd6d048
Yasss333 <yashmandhare3@gmail.com>
2026-01-16 15:09:33 +0530

    done with submisson controller

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//.md preview ctr+shift+v
+
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRouter from "../src/Routes/auth.routes.js";
+import probRouter from "../src/Routes/problemManagement.route.js"
+import executionRouter from "../src/Routes/execute_coderoutes.js"
+import submissionRoute from "../src/Routes/submission_route.js";
+dotenv.config();
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+  console.log("Hello from AlgoRank ✅ " );
+  res.send("Hello from AlgoRank ✅");
+ });
+
+ app.use("/api/v1/auth",authRouter);
+ app.use("/api/v1/problems",probRouter);
+ app.use("/api/v1/execute-route",executionRouter);
+ app.use("/api/v1/submission",submissionRoute)
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
