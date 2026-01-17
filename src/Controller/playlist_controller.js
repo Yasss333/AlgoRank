b1a6e714f7f08bca1091c054d0b7b6788fc5e8ed
Yasss333 <yashmandhare3@gmail.com>
2026-01-17 09:55:03 +0530

    added lieks , comments , and nested commenst in db

--- src/Controller/playlist_controller.js
+++ src/Controller/playlist_controller.js
@@ -24,17 +24,42 @@ export const createPlaylist= async (req,res) => {
  }
     
 }
+export const addProblemInPlaylist= async (req,res) => {
+    
+}
 export const getallproblemsfromPlaylist=async(req,res)=>{
-      
+      try {
+        const userID=req.user.id;
+        const problems=await db.playlist.findMany({
+            where:{
+                userID
+            },
+            include:{
+                problems:{
+                    include:{
+                        problem:true
+                    }
+                }
+            }
+        });
+
+        return res.status(200).json({
+          message:"All details/problems Fetched Successfully",
+          problems
+        })
+      } catch (error) {
+        console.error(error);
+        return res.status(200).json({
+            message:"Failed to get all details ",
+            error:error.message
+        })
+      }
 }
 
 export const getplaylistdetailsByPlaylistID=async (req,res) => {
     
 }
 
-export const addProblemInPlaylist= async (req,res) => {
-    
-}
 export const deletePlaylist= async (req,res) => {
     
 }
