9413e4ed86601dcc41983842358f85e64ea570c0
Yasss333 <yashmandhare3@gmail.com>
2026-01-23 22:26:25 +0530

    bad exp

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL + "/api/v1",
-//   withCredentials: true,
-// });
-
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : " import.meta.env.VITE_API_URL",
+  baseURL: import.meta.env.VITE_API_URL + "/api/v1",
   withCredentials: true,
 });
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : " import.meta.env.VITE_API_URL",
+//   withCredentials: true,
+// });
 
 // export const axiosInstance = axios.create({
 //   baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
