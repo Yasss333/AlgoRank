7ae10bd9d3cea7da0c4ce2ad951cff5d0ed62777
Yasss333 <yashmandhare3@gmail.com>
2026-01-23 11:46:36 +0530

    fiixng pre flight issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,25 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 
 // IMPORTANT: handle preflight explicitly
 
