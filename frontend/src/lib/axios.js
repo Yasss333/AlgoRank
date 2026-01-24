50bac301530f3e5679d6a91f2b1315c211bbdf4e
Yasss333 <yashmandhare3@gmail.com>
2026-01-24 08:32:49 +0530

    harrdcooed teh backedn url for lola host

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -18,7 +18,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL || 
+           `${import.meta.env.VITE_API_URL || "http://localhost:8080"}/api/v1`,
 });
 
 api.interceptors.request.use(
